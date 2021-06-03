e3d5b360b26325b3e87be7c414daf85a6846361b
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-06-04 01:20:42 +0530

    Added expand option for leave details

--- app/src/main/java/com/example/crm/LeaveManagement/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/crm/LeaveManagement/RecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package com.example.crm.LeaveManagement;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.transition.Slide;
+import androidx.transition.Transition;
+import androidx.transition.TransitionManager;
 
 import com.example.crm.Model.LeaveItem;
 import com.example.crm.R;
@@ -26,7 +31,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 	@NonNull
 	@Override
 	public LeaveItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-		View v = LayoutInflater.from(context).inflate(R.layout.leave_list_item, null, false);
+		View v = LayoutInflater.from(context).inflate(R.layout.leave_list_item, parent, false);
 		return new LeaveItemHolder(v);
 	}
 
@@ -41,8 +46,31 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 	}
 
 	public class LeaveItemHolder extends RecyclerView.ViewHolder {
+		ViewGroup details;
+		View expand;
+		ConstraintLayout container;
+
 		public LeaveItemHolder(@NonNull View itemView) {
 			super(itemView);
+			details = itemView.findViewById(R.id.details);
+			expand = itemView.findViewById(R.id.expand);
+			container = itemView.findViewById(R.id.container);
+
+			expand.setOnClickListener(v -> {
+
+				if (details.getVisibility() == View.GONE) {
+					expand.setRotation(180f);
+					Transition transition = new Slide(Gravity.BOTTOM);
+					transition.setDuration(300);
+					transition.addTarget(details);
+					TransitionManager.beginDelayedTransition(container, transition);
+					details.setVisibility(View.VISIBLE);
+				} else {
+					expand.setRotation(0f);
+					details.setVisibility(View.GONE);
+				}
+			});
+
 		}
 	}
 
