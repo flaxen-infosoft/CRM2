4379372061fdf71d12525db8540b82b93d5da73a
flaxen-infosoft <akshaythakur0308@gmail.com>
2021-06-10 19:16:45 +0530

    Changes in NewJoineeEmployee

--- app/src/main/java/com/example/crm/EmployeeManagement/NewJoineeEmployee.java
+++ app/src/main/java/com/example/crm/EmployeeManagement/NewJoineeEmployee.java
@@ -1,16 +1,20 @@
 package com.example.crm.EmployeeManagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.crm.R;
 
 public class NewJoineeEmployee extends AppCompatActivity {
-
+RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_joinee_employee);
+        recyclerView=findViewById(R.id.employee_newjoinee_recyler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(NewJoineeEmployee.this));
     }
 }
