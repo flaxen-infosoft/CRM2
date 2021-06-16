7f6e0985de129fe97754697058513eb3086e618f
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-06-17 04:16:39 +0530

    Fixes in Preferential candidate list adapter

--- app/src/main/java/com/example/crm/HRManagement/PreferenticalActivity.java
+++ app/src/main/java/com/example/crm/HRManagement/PreferenticalActivity.java
@@ -2,9 +2,11 @@ package com.example.crm.HRManagement;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import com.example.crm.Model.Candidate;
 import com.example.crm.R;
 import com.example.crm.Retro.RetroInterface;
 import com.example.crm.Retro.Retrofi;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
@@ -34,6 +37,7 @@ public class PreferenticalActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_preferentical);
 		rv = findViewById(R.id.recyclerView);
+		getPreferentialCandidate();
 	}
 
 	public void getPreferentialCandidate() {
@@ -46,6 +50,8 @@ public class PreferenticalActivity extends AppCompatActivity {
 			public void onResponse(Call<ArrayList<Candidate>> call, Response<ArrayList<Candidate>> response) {
 				if (response.isSuccessful()) {
 					candidates = response.body();
+					Gson gson = new Gson();
+					Log.e("123", gson.toJson(candidates));
 					updateUI();
 				} else {
 					CustomToast.makeText(PreferenticalActivity.this, "Server Error :(", 0, Color.RED);
@@ -65,14 +71,19 @@ public class PreferenticalActivity extends AppCompatActivity {
 
 		adapter = new PreferentialCandidateAdapter();
 		rv.setLayoutManager(new LinearLayoutManager(PreferenticalActivity.this));
-		CustomToast.makeText(PreferenticalActivity.this, "Server Error :(", 0, Color.RED);
+		rv.setAdapter(adapter);
 
 	}
 
 	public class PreferentialCandidateViewHolder extends RecyclerView.ViewHolder {
 
+		TextView name, applied_for, date, confirmationmail;
+
 		public PreferentialCandidateViewHolder(@NonNull View itemView) {
 			super(itemView);
+			name = itemView.findViewById(R.id.name);
+			applied_for = itemView.findViewById(R.id.applied_for);
+			date = itemView.findViewById(R.id.date);
 		}
 	}
 
@@ -88,6 +99,9 @@ public class PreferenticalActivity extends AppCompatActivity {
 		@Override
 		public void onBindViewHolder(@NonNull PreferentialCandidateViewHolder holder, int position) {
 
+			holder.name.setText(candidates.get(position).getName());
+			holder.applied_for.setText(candidates.get(position).getApplied_for());
+			holder.date.setText(candidates.get(position).getDateof_interview());
 		}
 
 		@Override
