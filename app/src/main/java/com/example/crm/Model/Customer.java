72ac95876c3da0f104de980a37ae1b2f3a526e0b
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-07-05 12:46:33 +0530

    get_customer_list API implementation

---
+++ app/src/main/java/com/example/crm/Model/Customer.java
@@ -0,0 +1,136 @@
+package com.example.crm.Model;
+
+public class Customer {
+
+	int id;
+	String name, domain, sub_domain, package_name, package_value, source, mail_id, phone_no, gender, state, city, address, priority, status, remark;
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public String getSub_domain() {
+		return sub_domain;
+	}
+
+	public void setSub_domain(String sub_domain) {
+		this.sub_domain = sub_domain;
+	}
+
+	public String getPackage_name() {
+		return package_name;
+	}
+
+	public void setPackage_name(String package_name) {
+		this.package_name = package_name;
+	}
+
+	public String getPackage_value() {
+		return package_value;
+	}
+
+	public void setPackage_value(String package_value) {
+		this.package_value = package_value;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getMail_id() {
+		return mail_id;
+	}
+
+	public void setMail_id(String mail_id) {
+		this.mail_id = mail_id;
+	}
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+}
