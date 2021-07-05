55cfc87716c5df067f1e843943d283fda2d42442
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-07-05 12:51:59 +0530

    get_customer_list API implementation

--- app/src/main/java/com/example/crm/CRMManagement/CustomersDetails.java
+++ app/src/main/java/com/example/crm/CRMManagement/CustomersDetails.java
@@ -1,31 +1,51 @@
 package com.example.crm.CRMManagement;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.crm.CustomToast;
+import com.example.crm.Model.Customer;
 import com.example.crm.R;
+import com.example.crm.Retro.RetroInterface;
+import com.example.crm.Retro.Retrofi;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CustomersDetails extends AppCompatActivity {
-    Spinner spinner;
-    RecyclerView recyclerView;
-    CardView cardView;
+	Spinner spinner;
+	RecyclerView recyclerView;
+	CardView cardView;
+	ArrayList<Customer> customers;
+	CustomerAdapter adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customers_details);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_customers_details);
 
-        cardView = findViewById(R.id.card);
-//        spinner = findViewById(R.id.customer_spinner);
-//        recyclerView=findViewById(R.id.customer_details_recycler);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		cardView = findViewById(R.id.card);
+		recyclerView = findViewById(R.id.customer_details_recycler);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		cardView.setOnClickListener(v -> startActivity(new Intent(CustomersDetails.this, CustomerDetails.class)));
 
-        cardView.setOnClickListener(v -> startActivity(new Intent(CustomersDetails.this, CustomerDetails.class)));
+		fetchCustomers();
 
 //        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"Manage Customer", "View Customers"}));
 //
@@ -35,5 +55,63 @@ public class CustomersDetails extends AppCompatActivity {
 //        stringList.add("1");
 
 //        recyclerView.setAdapter(new CustomerAdapter(stringList));
-    }
+
+
+	}
+
+	private void fetchCustomers() {
+		RetroInterface ri = Retrofi.initretro().create(RetroInterface.class);
+		Call<ArrayList<Customer>> call = ri.getCustomers();
+
+		call.enqueue(new Callback<ArrayList<Customer>>() {
+			@Override
+			public void onResponse(Call<ArrayList<Customer>> call, Response<ArrayList<Customer>> response) {
+				if (response.isSuccessful()) {
+					customers = response.body();
+					updateUI();
+				} else {
+					CustomToast.makeText(CustomersDetails.this, "Failed: " + response.message(), 0, Color.RED);
+				}
+			}
+
+			@Override
+			public void onFailure(Call<ArrayList<Customer>> call, Throwable t) {
+				CustomToast.makeText(CustomersDetails.this, "Failed: " + t.getMessage(), 0, Color.RED);
+			}
+		});
+
+	}
+
+	private void updateUI() {
+		adapter = new CustomerAdapter();
+		recyclerView.setAdapter(adapter);
+
+	}
+
+	private class CustomerViewHolder extends RecyclerView.ViewHolder {
+		public CustomerViewHolder(@NonNull @NotNull View itemView) {
+			super(itemView);
+		}
+	}
+
+	private class CustomerAdapter extends RecyclerView.Adapter<CustomerViewHolder> {
+
+		@NonNull
+		@NotNull
+		@Override
+		public CustomerViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+			View v = LayoutInflater.from(CustomersDetails.this).inflate(R.layout.customer_list_item, parent, false);
+			return new CustomerViewHolder(v);
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull @NotNull CustomerViewHolder holder, int position) {
+
+		}
+
+		@Override
+		public int getItemCount() {
+			return customers.size();
+		}
+	}
 }
