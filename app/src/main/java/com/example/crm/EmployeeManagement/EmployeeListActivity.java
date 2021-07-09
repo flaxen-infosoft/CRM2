3eb1a1ec05a3fabfacda328ba06a4f31c6b82c92
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2021-07-09 15:17:19 +0530

    Profile image in EmployeeListActivity

--- app/src/main/java/com/example/crm/EmployeeManagement/EmployeeListActivity.java
+++ app/src/main/java/com/example/crm/EmployeeManagement/EmployeeListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,11 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.crm.Model.Employee;
 import com.example.crm.R;
 import com.example.crm.Retro.RetroInterface;
 import com.example.crm.Retro.Retrofi;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
@@ -26,93 +27,90 @@ import retrofit2.Response;
 
 public class EmployeeListActivity extends AppCompatActivity {
 
-	ArrayList<Employee> employees;
+    ArrayList<Employee> employees;
 
-	RecyclerView rv;
-	EmployeeAdapter adapter;
+    RecyclerView rv;
+    EmployeeAdapter adapter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_employee_list);
-		employees = new ArrayList<>();
-		rv = findViewById(R.id.recyclerView);
-		adapter = new EmployeeAdapter();
-		rv.setLayoutManager(new LinearLayoutManager(this));
-		rv.setAdapter(adapter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_list);
+        employees = new ArrayList<>();
+        rv = findViewById(R.id.recyclerView);
+        adapter = new EmployeeAdapter();
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
 
+        //TODO Create API to fetch all employees and show them here => dummy employees
 
-		//TODO Create API to fetch all employees and show them here => dummy employees
+        fetch();
+    }
 
-		fetch();
-	}
+    private void fetch() {
+        RetroInterface ri = Retrofi.initretro().create(RetroInterface.class);
+        Call<ArrayList<Employee>> call = ri.getEmployee();
+        call.enqueue(new Callback<ArrayList<Employee>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Employee>> call, Response<ArrayList<Employee>> response) {
+                if (!response.isSuccessful()) {
+                    System.out.println("response = " + response.code());
+                }
+                employees.addAll(response.body());
+                adapter.notifyDataSetChanged();
+            }
 
-	private void fetch() {
-		RetroInterface ri = Retrofi.initretro().create(RetroInterface.class);
-		Gson gson = new Gson();
-		Call<ArrayList<Employee>> call = ri.getEmployee();
-		call.enqueue(new Callback<ArrayList<Employee>>() {
-			@Override
-			public void onResponse(Call<ArrayList<Employee>> call, Response<ArrayList<Employee>> response) {
-				if (!response.isSuccessful()) {
-					System.out.println("response = " + response.code());
-				}
-				employees.addAll(response.body());
-				adapter.notifyDataSetChanged();
-			}
-
-			@Override
-			public void onFailure(Call<ArrayList<Employee>> call, Throwable t) {
-				System.out.println("t = " + t.getMessage());
-			}
-		});
-	}
+            @Override
+            public void onFailure(Call<ArrayList<Employee>> call, Throwable t) {
+                System.out.println("t = " + t.getMessage());
+            }
+        });
+    }
 
 
-	public class EmployeeViewHolder extends RecyclerView.ViewHolder {
+    public class EmployeeViewHolder extends RecyclerView.ViewHolder {
 
-		TextView name, date, dept;
+        TextView name, date, dept;
+        ImageView img;
 
-		public EmployeeViewHolder(@NonNull View itemView) {
-			super(itemView);
-			name = itemView.findViewById(R.id.name);
-			date = itemView.findViewById(R.id.date);
-			dept = itemView.findViewById(R.id.dept);
+        public EmployeeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            date = itemView.findViewById(R.id.date);
+            dept = itemView.findViewById(R.id.dept);
+            img = itemView.findViewById(R.id.img);
 
-			itemView.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					int pos = getAdapterPosition();
-					Intent i = new Intent(EmployeeListActivity.this, ActivityNewJoinee.class);
-					i.putExtra("employee", employees.get(pos));
-					startActivity(i);
-				}
-			});
+            itemView.setOnClickListener(v -> {
+                int pos = getAdapterPosition();
+                Intent i = new Intent(EmployeeListActivity.this, ActivityNewJoinee.class);
+                i.putExtra("employee", employees.get(pos));
+                startActivity(i);
+            });
 
+        }
+    }
 
-		}
-	}
+    public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeViewHolder> {
 
-	public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeViewHolder> {
+        @NonNull
+        @Override
+        public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(EmployeeListActivity.this).inflate(R.layout.employee_list_item, parent, false);
+            return new EmployeeViewHolder(v);
+        }
 
-		@NonNull
-		@Override
-		public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-			View v = LayoutInflater.from(EmployeeListActivity.this).inflate(R.layout.employee_list_item, parent, false);
-			return new EmployeeViewHolder(v);
-		}
+        @Override
+        public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
+            holder.name.setText(employees.get(position).getName());
+            holder.date.setText(employees.get(position).getDate());
+            holder.dept.setText(employees.get(position).getDepartment());
+            Glide.with(EmployeeListActivity.this).load(employees.get(position).getProfileImg()).into(holder.img);
+        }
 
-		@Override
-		public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
-			holder.name.setText(employees.get(position).getName());
-			holder.date.setText(employees.get(position).getDate());
-			holder.dept.setText(employees.get(position).getDepartment());
-		}
-
-		@Override
-		public int getItemCount() {
-			return employees.size();
-		}
-	}
+        @Override
+        public int getItemCount() {
+            return employees.size();
+        }
+    }
 
 }
