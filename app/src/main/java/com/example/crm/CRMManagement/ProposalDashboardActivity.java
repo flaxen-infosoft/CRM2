e3259d3e911eb87e38e544987841652435c9815e
meetvani <meetvani6@gmail.com>
2021-07-12 15:46:17 +0530

    Added ProposalDashboardActivity

---
+++ app/src/main/java/com/example/crm/CRMManagement/ProposalDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.example.crm.CRMManagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.crm.R;
+
+public class ProposalDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposal_dashboard);
+    }
+}
