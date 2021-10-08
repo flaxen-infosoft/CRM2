4685217b1abb8033af5311f0cfccb9b59113c642
AAYUSH <aayushmalviya202@gmail.com>
2021-10-08 11:55:22 +0530

    salesreport and everything except meeting

--- app/src/main/java/com/example/crm/SalesManagement/Meetingnew.java
+++ app/src/main/java/com/example/crm/SalesManagement/Meetingnew.java
@@ -1,11 +1,11 @@
 package com.example.crm.SalesManagement;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.crm.R;
 import com.google.android.material.card.MaterialCardView;
@@ -30,7 +30,6 @@ MaterialCardView ongoing,infuture;
     {ongoing=findViewById(R.id.currenttt);
         infuture=findViewById(R.id.notintr);
         infuture.setCardBackgroundColor(getColor(R.color.aayushpurple));
-        ongoing.setCardBackgroundColor(getColor(R.color.aayushthemepurple));
-
-    }
+       ongoing.setCardBackgroundColor(getColor(R.color.aayushthemepurple));
+     }
 }
